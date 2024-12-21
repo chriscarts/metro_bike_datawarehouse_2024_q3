b59aab14a6c41a649e5a3033d03ec6059c21d9ab
chriscarts <christian.cartagena@baruchmail.cuny.edu>
2024-12-21 01:47:12 -0500

    Delete testing_queries directory

--- testing_queries/average_trip_duration.sql
+++
@@ -1,5 +0,0 @@
-SELECT station_name, AVG(duration) AS avg_duration
-FROM `cosmic-sensor-445301-v1.data_warehouse.fact_bike_trips`
-GROUP BY station_name
-ORDER BY avg_duration DESC
-LIMIT 10;
--- testing_queries/trip_peak_time.sql
+++
@@ -1,5 +0,0 @@
-SELECT trip_date, COUNT(*) AS total_trips
-FROM `cosmic-sensor-445301-v1.data_warehouse.fact_bike_trips`
-GROUP BY trip_date
-ORDER BY total_trips DESC
-LIMIT 10;
