4c39d4d8886f6e0a18f9ac7af90040d316856b4d
chriscarts <christian.cartagena@baruchmail.cuny.edu>
2024-12-21 08:58:06 -0500

    Delete testing_queries directory

--- testing_queries/top_5_busiest_stations.sql
+++
@@ -1,5 +0,0 @@
-SELECT start_station, COUNT(*) AS trip_count
-FROM `cosmic-sensor-445301-v1.data_warehouse.bike_share_trips_2024_q3`
-GROUP BY start_station
-ORDER BY trip_count DESC
-LIMIT 5;
--- testing_queries/trips_over_time.sql
+++
@@ -1,4 +0,0 @@
-SELECT trip_date, COUNT(trip_id) AS trip_count
-FROM `cosmic-sensor-445301-v1.data_warehouse.fact_bike_trips`
-GROUP BY trip_date
-ORDER BY trip_date;
